c0bab20d5b8dce2b44124b752ea8177de46ed541
Yassine EL-BADRAOUI <112555529+yassineiscoding@users.noreply.github.com>
2024-10-09 18:49:42 +0100

    Time: 0 ms (100%), Space: 40.6 MB (74.18%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,11 +1,10 @@
 class Solution {
     public static int reverse(int x) {
         int reverse = 0;
-        int quotient = x;
-        while(quotient != 0){
+        while(x != 0){
             if(reverse > Integer.MAX_VALUE / 10 || reverse < Integer.MIN_VALUE / 10) return 0;
-            reverse = reverse * 10 + quotient % 10;
-            quotient = quotient / 10;
+            reverse = reverse * 10 + x % 10;
+            x = x / 10;
         }
         return reverse;
     }
